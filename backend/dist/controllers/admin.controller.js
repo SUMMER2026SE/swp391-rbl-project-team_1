ebeb8a7d20572bd2807d6d3aa1fa53baf0e7032b
Dung Nguyen <heotaixanh121212@gmail.com>
2026-06-22 22:07:16 +0700

    chore: xóa toàn bộ dữ liệu

--- backend/dist/controllers/admin.controller.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.adminPlaceholder = void 0;
-exports.adminPlaceholder = {};
--- backend/prisma/migrations/20260527063541_add_profile_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "address" TEXT,
-ADD COLUMN     "avatar" TEXT,
-ADD COLUMN     "dateOfBirth" TIMESTAMP(3),
-ADD COLUMN     "fullName" TEXT,
-ADD COLUMN     "gender" TEXT;
